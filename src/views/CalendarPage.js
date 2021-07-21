a3d7d8e332d361e310b62c6358f9d60a23eed3ef
Dynaste <maximemary.pro@gmail.com>
2021-07-21 19:42:28 +0200

    add subtitle

--- src/views/CalendarPage.js
+++ src/views/CalendarPage.js
@@ -1,24 +1,51 @@
 import { Agenda, Calendar, CalendarList } from "react-native-calendars";
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
-import { borderRadiusValue, classicBackground, contrastBackground, displayDim, distanceBetween2Element, mainColor } from "./../../helpers/cssValues";
+import {
+  borderRadiusValue,
+  classicBackground,
+  contrastBackground,
+  displayDim,
+  distanceBetween2Element,
+  mainColor,
+} from "./../../helpers/cssValues";
 
-import {LocaleConfig} from 'react-native-calendars';
+import { LocaleConfig } from "react-native-calendars";
 import React from "react";
 
 const CalendarPAge = ({ navigation }) => {
-
-  LocaleConfig.locales['fr'] = {
-    monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
-    dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
-    dayNamesShort: ['D.','L.','M.','M.','J.','V.','S.'],
-    today: 'Aujourd\'hui'
+  LocaleConfig.locales["fr"] = {
+    monthNames: [
+      "Janvier",
+      "Février",
+      "Mars",
+      "Avril",
+      "Mai",
+      "Juin",
+      "Juillet",
+      "Août",
+      "Septembre",
+      "Octobre",
+      "Novembre",
+      "Décembre",
+    ],
+    dayNames: [
+      "Dimanche",
+      "Lundi",
+      "Mardi",
+      "Mercredi",
+      "Jeudi",
+      "Vendredi",
+      "Samedi",
+    ],
+    dayNamesShort: ["D.", "L.", "M.", "M.", "J.", "V.", "S."],
+    today: "Aujourd'hui",
   };
-  LocaleConfig.defaultLocale = 'fr';
+  LocaleConfig.defaultLocale = "fr";
   return (
     <>
       <SafeAreaView style={{ backgroundColor: classicBackground }}>
         <ScrollView style={styles.main}>
-        <View style={styles.titleContainer}>
+          <View style={styles.titleContainer}>
             <Text style={styles.title}>Mon calendrier</Text>
             <View style={styles.underline}></View>
           </View>
@@ -57,31 +84,35 @@ const CalendarPAge = ({ navigation }) => {
                 borderColor: mainColor,
                 borderRadius: borderRadiusValue,
                 paddingBottom: distanceBetween2Element,
-                paddingTop: distanceBetween2Element
+                paddingTop: distanceBetween2Element,
               }}
               theme={{
                 calendarBackground: contrastBackground,
                 textSectionTitleColor: mainColor,
-                textSectionTitleDisabledColor: '#d9e1e8',
-                selectedDayBackgroundColor: '#00adf5',
-                selectedDayTextColor: '#ffffff',
+                textSectionTitleDisabledColor: "#d9e1e8",
+                selectedDayBackgroundColor: "#00adf5",
+                selectedDayTextColor: "#ffffff",
                 todayTextColor: mainColor,
-                dayTextColor: 'black',
-                textDisabledColor: '#d9e1e8',
-                dotColor: '#00adf5',
-                selectedDotColor: '#ffffff',
+                dayTextColor: "black",
+                textDisabledColor: "#d9e1e8",
+                dotColor: "#00adf5",
+                selectedDotColor: "#ffffff",
                 arrowColor: mainColor,
-                disabledArrowColor: '#d9e1e8',
-                monthTextColor: 'black',
-                indicatorColor: 'black',
-                textDayFontWeight: '300',
-                textMonthFontWeight: 'bold',
-                textDayHeaderFontWeight: '300',
+                disabledArrowColor: "#d9e1e8",
+                monthTextColor: "black",
+                indicatorColor: "black",
+                textDayFontWeight: "300",
+                textMonthFontWeight: "bold",
+                textDayHeaderFontWeight: "300",
                 textDayFontSize: 20,
                 textMonthFontSize: 18,
-                textDayHeaderFontSize: 18
+                textDayHeaderFontSize: 18,
               }}
             />
+          </View>
+          <View style={styles.subtitleContainer}>
+            <Text style={styles.subtitle}>Mes prochains évènements</Text>
+            <View style={styles.underline}></View>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -100,8 +131,8 @@ const styles = StyleSheet.create({
   },
   calendarContainer: {
     marginTop: distanceBetween2Element,
-    width: displayDim.x-40,
-    marginLeft: distanceBetween2Element
+    width: displayDim.x - 40,
+    marginLeft: distanceBetween2Element,
   },
   titleContainer: {
     marginLeft: distanceBetween2Element,
@@ -109,6 +140,15 @@ const styles = StyleSheet.create({
     width: 139,
   },
   title: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  subtitleContainer: {
+    marginLeft: distanceBetween2Element,
+    marginTop: distanceBetween2Element,
+    width: 255,
+  },
+  subtitle: {
     fontSize: 20,
     fontWeight: "600",
   },
