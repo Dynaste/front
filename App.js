1dd121d4a0e56f5f9c9ac4deb1a4f345dc9f94e6
Dynaste <maximemary.pro@gmail.com>
2021-07-21 17:11:56 +0200

    Refact app done

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
-import { Image, StyleSheet, View } from "react-native";
-import {
-  classicBackground,
-  contrastBackground,
-  mainColor,
-} from "./helpers/cssValues";
 import { combineReducers, createStore } from "redux";
 import { persistReducer, persistStore } from "redux-persist";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import CalendarPage from "./src/views/CalendarPage";
-import HomePage from "./src/views/HomePage";
 import { NavigationContainer } from "@react-navigation/native";
 import NavigatorPartyCreation from './src/components/NavigatorPartyCreation';
-import NotificationPage from "./src/views/NotificationPage";
-import PartyCreationPage from "./src/views/PartyCreationPage";
 import { PersistGate } from "redux-persist/integration/react";
-import ProfilPage from "./src/views/ProfilPage";
 import { Provider } from "react-redux";
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import tokenReducer from "./src/reducers/tokenReducer";
 
 export default function App() {
-  const Tab = createBottomTabNavigator();
 
   const tokenPersistConfig = {
     key: "root",
@@ -46,33 +33,4 @@ export default function App() {
       </Provider>
     </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: classicBackground,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  shadow: {
-    shadowColor: mainColor,
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.5,
-    elevation: 5,
-  },
-  iconContainer: {
-    top: 15,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  icon: {
-    width: 25,
-    height: 25,
-    tintColor: mainColor,
-  },
-});
+};
