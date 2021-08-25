c7884fa9b456474ac460a76c33c598eefff15284
Dynaste <maximemary.pro@gmail.com>
2021-08-25 10:34:07 +0200

    Show pseudo on profil page

--- src/views/ProfilPage.js
+++ src/views/ProfilPage.js
@@ -4,20 +4,20 @@ import {
   StyleSheet,
   Switch,
   Text,
-  View
+  View,
 } from "react-native";
 import {
   defaultTextFontWeight,
   displayDim,
   distanceBetween2Element,
-  mainColor
+  mainColor,
 } from "../../helpers/cssValues";
 import { useDispatch, useSelector } from "react-redux";
 
 import Login from "./../components/Login";
 import React from "react";
-import { isLog } from "./../../helpers/functions";
 import { getLoggedInUser } from "../../helpers/api";
+import { isLog } from "./../../helpers/functions";
 
 const ProfilPage = ({ navigation }) => {
   const identity = useSelector((state) => state.tokenRedux);
@@ -28,26 +28,76 @@ const ProfilPage = ({ navigation }) => {
   const init = async (token) => {
     const user = await getLoggedInUser(token);
     setUser(user.data);
-    console.log({user});
-  }
+    console.log({ user });
+  };
 
   React.useEffect(() => {
     console.log(identity);
 
-    if (identity.jwt !== '') {
+    if (identity.jwt !== "") {
       init(identity.jwt);
     }
   }, [identity]);
 
   return (
-    <SafeAreaView style={[styles.main, {backgroundColor: theme.background}]}>
+    <SafeAreaView style={[styles.main, { backgroundColor: theme.background }]}>
       {!isLog(identity) ? (
         <Login navigation={navigation} />
       ) : (
         <>
-          <Text style={{ marginTop: distanceBetween2Element, fontSize: 24, color: theme.fontColor}}>
+          <Text
+            style={{
+              marginTop: distanceBetween2Element,
+              fontSize: 24,
+              color: theme.fontColor,
+            }}
+          >
             Vous êtes connecté
           </Text>
+          <Text
+            style={{
+              marginTop: distanceBetween2Element,
+              fontSize: 20,
+              color: theme.fontColor,
+            }}
+          >
+            {userDetail.username}
+          </Text>
+          {/* <Text
+            style={{
+              marginTop: distanceBetween2Element,
+              fontSize: 20,
+              color: theme.fontColor,
+            }}
+          >
+            Nom: {userDetail.lastname}
+          </Text>
+          <Text
+            style={{
+              marginTop: distanceBetween2Element,
+              fontSize: 20,
+              color: theme.fontColor,
+            }}
+          >
+            Prénom: {userDetail.firstname}
+          </Text> */}
+          <View style={styles.toggleContainer}>
+            <Text style={[styles.label, { color: theme.fontColor }]}>
+              Thème sombre :
+            </Text>
+            <Switch
+              trackColor={{ false: "#767577", true: mainColor }}
+              thumbColor={theme.darkMode ? "#3e3e3e" : "#f4f3f4"}
+              ios_backgroundColor="#767577"
+              onValueChange={() =>
+                dispatch({
+                  type: "SET_THEME",
+                  payload: { theme: !theme.darkMode },
+                })
+              }
+              value={theme.darkMode}
+            />
+          </View>
           <Pressable
             style={{
               borderRadius: 10,
@@ -61,18 +111,16 @@ const ProfilPage = ({ navigation }) => {
             }}
             onPress={() => dispatch({ type: "reset_jwt", payload: {} })}
           >
-            <Text style={{ color: "#fff", fontSize: 20, fontWeight: defaultTextFontWeight}}>Logout</Text>
+            <Text
+              style={{
+                color: "#fff",
+                fontSize: 20,
+                fontWeight: defaultTextFontWeight,
+              }}
+            >
+              Logout
+            </Text>
           </Pressable>
-          <View style={styles.toggleContainer}>
-            <Text style={[styles.label, {color: theme.fontColor}]}>Thème sombre :</Text>
-            <Switch
-              trackColor={{ false: "#767577", true: mainColor }}
-              thumbColor={theme.darkMode ? "#3e3e3e" : "#f4f3f4"}
-              ios_backgroundColor="#767577"
-              onValueChange={() => dispatch({ type: "SET_THEME", payload: {theme: !theme.darkMode} })}
-              value={theme.darkMode}
-            />
-          </View>
         </>
       )}
     </SafeAreaView>
@@ -86,18 +134,19 @@ const styles = StyleSheet.create({
     minWidth: displayDim.x,
     height: displayDim.y,
     alignItems: "center",
+    // justifyContent: "center"
   },
   toggleContainer: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "flex-start",
     alignItems: "center",
-    marginTop: distanceBetween2Element
+    marginTop: distanceBetween2Element,
   },
   label: {
     marginRight: distanceBetween2Element,
-    fontWeight: defaultTextFontWeight
-  }
+    fontWeight: defaultTextFontWeight,
+  },
 });
 
 export default ProfilPage;
